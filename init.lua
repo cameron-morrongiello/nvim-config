2bc80e0c405e4ce2fe68b41f83eb90af1be38fe6
Cameron <cameron@Mac.attlocal.net>
2024-12-12 11:08:50 -0600

    first commit

---
+++ init.lua
@@ -0,0 +1,22 @@
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+-- The MacOS default terminal does not support many types of colorso lets turn it off and use a default color scheme (I think they work...)
+vim.opt.termguicolors = false
+vim.cmd('colorscheme sorbet')
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
